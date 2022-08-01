6368f03ce3a4d736f24c86a61f12d104fc8cc741
joeephwild <josephomotade0@gmail.com>
2022-08-01 16:33:24 +0000

    comm

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -8,12 +8,12 @@ function Banner() {
   return (
     <div 
     style={{ fontFamily: "Montserrat" }} 
-     className='text-8xl w-full h-screen relative flex flex-col py-4 font-'>
+     className='text-8xl w-full h-screen break-before-column relative flex flex-col py-4 font-'>
       <span className="font-bold">DEVELOPER</span>
       <Image src={image1} className="rounded-2xl mx-auto" width={3200} height={3200} objectFit="cover" alt="" />
       <span className='ml-48'>WITH USERS</span>
       <span className='text-center'>IN <span className="font-bold">MIND</span></span>
-      <div style={{ fontFamily: "Open Sans"}} className="text-lg mx-auto">
+      <div style={{ fontFamily: "Open Sans"}} className="text-lg text-center md:mx-auto">
         <span>Aspiring movie content creator turned develoer, now based in Lagos Nigeria.</span>
       </div>
       
