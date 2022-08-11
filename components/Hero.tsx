8be0c9e8cebac568dc8cb4d339a783c4398a6d85
joeephwild <josephomotade0@gmail.com>
2022-08-11 06:11:48 +0000

    commit

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -33,23 +33,23 @@ function Hero() {
       <div  className="flex flex-col">
         <div  className="flex justify-between items-center p-4">
           <span className="md:text-8xl text-7xl  font-thin font-raleway">THE</span>
-            <span className="text-sm hidden md:block text-[#fafafa] w-48">one of the largest and most influential museums of the modern art in the word</span>
-          <span className="text-sm  hidden md:block text-[#fafafa] w-60">New York City, 53rd Street</span>
+            <span className="hidden md:block text-xs text-[#fafafa] w-48">one of the largest and most influential museums of the modern art in the word</span>
+          <span className="text-xs  hidden md:block text-[#fafafa] w-60">New York City, 53rd Street</span>
         </div>
       </div>
 
       {/* top-section end */}
-      <motion.div variants={parentVariant} className="relative">
-      <span className="md:text-8xl text-7xl  flex font-thin justify-center items-end font-raleway md:right-0 md:absolute">MUSEUM</span>
+      <motion.div variants={parentVariant} className="">
+      <span className="md:text-8xl text-7xl  flex font-thin justify-center items-end font-raleway md:pl-[50%]">MUSEUM</span>
       </motion.div>
       {/* 3rd text  */}
-      <motion.div variants={parentVariant} className="relative">
-      <span className="md:text-8xl text-7xl  absolute md:top-14 top-0 font-thin font-raleway left-16 md:left-0">OF</span>
+      <motion.div variants={parentVariant} className="">
+      <span className="md:text-8xl text-7xl pl-[10%] md:pl-[2%]  font-thin font-raleway">OF</span>
       </motion.div>
 
-      <motion.div variants={parentVariant} className="relative ">
-          <div className="absolute  top-20 md:top-40 space-x-28">
-          <span className="md:text-8xl flex items-center text-7xl font-thin font-raleway">
+      <motion.div variants={parentVariant} className="">
+          <div className=" space-x-28">
+          <span className="md:text-8xl pl-[30%] lg:pl-[10%] flex items-center text-7xl font-thin font-raleway">
             <span className="text-xs text-[#fafafa] mr-28 pt-16 text-start hidden md:block">since 1929 </span>MORDERN ART</span>
         </div>
       </motion.div>
